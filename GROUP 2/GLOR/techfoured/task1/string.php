de4c67ef267357cccb92ec52fa1e24a06f690809
maricarglor02@gmail.com <maricarglor22@gmail.com>
2019-12-06 15:08:50 +0700

    glor
    task

---
+++ GROUP 2/GLOR/techfoured/task1/string.php
@@ -0,0 +1,19 @@
+<?php
+$email = 'john.smith@gmail.com';
+$user = strstr($email, '@', true);
+echo $user."\n";
+?>
+<br/>
+
+<?php
+$email = 'sweet_six16@mcl.edu.php';
+$user = strstr($email, '@', true);
+echo $user."\n";
+?>
+<br/>
+
+<?php
+$email = 'yorme_isko@yahoo.com.ph';
+$user = strstr($email, '@', true);
+echo $user."\n";
+?>

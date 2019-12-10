2ca948ec2e84cf4ba6c8d48929b3b584cfe96c08
Kerby Patungan <kerbypatungan08@gmail.com>
2019-12-10 08:57:43 +0800

    kerby
    edit some codes

--- GROUP 1/PATUNGAN/Task2/task2.php
+++ GROUP 1/PATUNGAN/Task2/task2.php
@@ -3,11 +3,15 @@
 <head>
 <meta charset="UTF-8">
 <title> CALCULATOR </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
+<div>
+<center>
 <form>
+	<form style="max-width: 450px; margin: auto;">
 	<input type="text" name="num1" placeholder="Number 1">
 	<input type="text" name="num2" placeholder="Number 2">
 	<select name="operator">
@@ -47,7 +51,9 @@
 	}	
 ?>
 
-
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</div>
 </body>
 
 </html>

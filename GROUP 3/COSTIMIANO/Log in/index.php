66bb7493d8f5d48af0dd577acdd3f271e6ea1218
Kevin Costimiano <costimianokevin19@gmail.com>
2019-12-10 17:03:28 +0800

    costimiano
    tapos na!

---
+++ GROUP 3/COSTIMIANO/Log in/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+<body>
+
+<div class="container">
+<br/>
+<h1 class="text-center">LOG IN TO CONTINUE</h1>
+ <form method="POST" action="function.php">
+		        Username <input type="text" name="username" placeholder="Enter username" class="form-control">
+				Password <input type="password" name="password" placeholder="Enter password" class="form-control">
+				<br><center>
+				<input type="submit" value="LOG IN" name="log in" class="btn btn-primary btn-block">
+		   </form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html> 

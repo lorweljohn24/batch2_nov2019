808473bdac54fbbd684d6fe9e696ee8b753bbb7b
AngeleneLacson <lacsonangelene20@gmail.com>
2019-12-11 16:28:08 +0800

    Lacson
    CATCHAP

--- GROUP 2/LACSON/system.php
+++ GROUP 2/LACSON/system.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="style.css">
 
 <body>
-<body background="ge.jpg">
+<body background="pink.jpg">
 
 <div class = "container">
 <br/>
@@ -15,6 +15,91 @@
 <br/>
 <input class = "form-control" type="password" placeholder = "password">
 <br/>
+<table  border="0" align="center">
+
+    <tr>
+
+      <td width="215"> Validation code:</td>
+
+      <td width="162">
+
+<?php
+
+$arr= array_merge(range(0,9),range("A","Z"));
+
+//print_r($arr);
+
+for($i=1;$i<=5;$i++)
+
+{
+	$ch = $arr[array_rand($arr)];
+
+	@$captcha=$captcha.$ch;
+
+	@$fc=$fc.$ch.",";
+
+}
+
+//echo $fc."<br>";
+$nar = explode(",",$fc);
+
+for($i=0;$i<5;$i++)
+
+{
+
+echo $nar[$i];
+//echo "<img src='$nar[$i].GIF'/>";
+
+}
+
+if(isset($_POST['match']))
+
+{
+
+    if($_POST['img']==$_POST['hid'])
+
+     {
+
+      echo  "<br/><font color='blue'>security code matched</font>";
+
+     }
+
+    else
+
+    {
+
+	echo "<br/><font color='red'> try again</font>";
+
+    }
+
+}
+
+?>
+
+    <tr>
+
+        <td>Enter the code here :</td>
+
+       <td> <input name="img" type="text">
+
+        </td>
+
+    </tr>
+
+    <tr>
+
+      <td colspan="2" align="center">
+
+	  
+
+    </tr>
+
+  </table>
+
+<input type="hidden" value="<?php echo $captcha; ?>" name="hid"/>
+<br/>
+<form action = "session.php" method = "POST" style = "max-width: 450px; margin: auto;">
+
 <button type="submit" class="btn btn-primary btn-block">Log in</button>
 </form>
 </div>

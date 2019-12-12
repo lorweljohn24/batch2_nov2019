4715f0f5b28f9fac0c17dff5d12e1a913afc4863
Jenny Holaso <jennyroseholaso16@gmail.com>
2019-12-12 16:57:33 +0800

    jenny
    captcha

---
+++ GROUP 1/HOLASO/login/loginform.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></script>
+<link rel="stylesheet" href="style.css">
+
+<body>
+
+
+<div class = "container">
+<br/>
+<h1 class = "text-center">LOG IN TO CONTINUE</h1>
+<form action = "functions.php" method = "POST" style = "max-width: 450px; margin: auto;">
+	Username <input type = "text" name = "username" placeholder = "Enter username" class = "form-control">
+	Password <input type = "password" name = "password" placeholder = "Enter password" class = "form-control">
+	<table  border="0" align="center">
+
+    <tr>
+
+      <td width="200"> code:</td>
+
+      <td width="160">
+
+<?php
+
+$arr= array_merge(range(0,9),range("A","Z"));
+
+//print_r($arr);
+
+for($i=1;$i<=5;$i++)
+
+{
+	$ch = $arr[array_rand($arr)];
+
+	@$captcha=$captcha.$ch;
+
+	@$fc=$fc.$ch.",";
+
+}
+
+//echo $fc."<br>";
+$nar = explode(",",$fc);
+
+for($i=0;$i<5;$i++)
+
+{
+
+echo $nar[$i];
+//echo "<img src='$nar[$i].GIF'/>";
+
+}
+
+if(isset($_POST['match']))
+
+{
+
+    if($_POST['img']==$_POST['hid'])
+
+     {
+
+      echo  "<br/><font color='black'>security code matched</font>";
+
+     }
+
+    else
+
+    {
+
+	echo "<br/><font color='red'> try again</font>";
+
+    }
+
+}
+
+?>
+
+    <tr>
+
+        <td>Enter the  above code here :</td>
+
+       <td> <input name="img" type="text">
+
+        </td>
+
+    </tr>
+
+    <tr>
+
+      <td colspan="2" align="center">
+
+	 
+
+    </tr>
+
+  </table>
+
+<input type="hidden" value="<?php echo $captcha; ?>" name="hid"/>
+<br/>
+<button type="submit" class="btn btn-primary btn-block">Log in</button>
+</form>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+<style>
+body {
+  background-image: url('sis.jpg');
+}
+</style>

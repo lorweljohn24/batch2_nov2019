1db92b1bf0b163a14c1b2a6d361cac038df6e3d6
shaina15 <58279783+shaina015@users.noreply.github.com>
2019-12-12 17:06:51 +0800

    taskkk
    taskkk

---
+++ GROUP 3/CATALAN/taskkk/new 1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+<body>
+
+<div class="container">
+<div class="row">
+<div class="col-lg-3"> </div>
+<div class="col-lg-6">
+<br/>
+<h1 class="text-center">LOG IN TO CONTINUE</h1>
+ <form action="function.php"  method="POST" >
+		        Username <input type="text" name="username" placeholder="Enter username" class="form-control">
+				Password <input type="password" name="password" placeholder="Enter password" class="form-control">
+				<table  border="0" align="center">
+				<html>
+
+<title>Contact Us Form</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+<form name="frmContact" method="post" action="">
+<div class="message"><?php if(isset($message)) { echo $message; } ?></div>
+<table border="0" cellpadding="10" cellspacing="1" width="500" align="center">
+<tr class="tablerow">
+<td colspan="2">Captcha Code<br/><input name="captcha_code" type="text"><br>
+YFK86
+</tr>
+</tr>
+</table>
+</body>
+
+				<input type="submit" value="LOG IN" name="log in" class="btn btn-primary btn-block">
+		   </form>		  		  
+</div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html> 
+
+<style>
+body {
+	background-image: url("pink.jpg");
+}
+</style>

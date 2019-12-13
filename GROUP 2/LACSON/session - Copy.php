71fee5148334fedc3423f46114a6358848ca4f7b
AngeleneLacson <lacsonangelene20@gmail.com>
2019-12-13 15:49:01 +0800

    LACSON20
    LAST NA

---
+++ GROUP 2/LACSON/session - Copy.php
@@ -0,0 +1,78 @@
+<?php 
+	session_start();
+	
+	echo '<h1 class = "text-center">The Fisherman and The Jinni</h1>';
+	echo '<h2 class = "text-center"><h2>';
+	echo 'There was a poor fisherman, who had a wife and three children in his family. He used to cast his net in seawater to catch fish for his livelihood. He had made a rule to cast his net only four times a day and never exceeded the set limit.
+
+One day the fisherman went on his day’s work. He threw his net in the seawater. After sometime when he tried to pull it out, he could not do so. He was overjoyed thinking that some big fish fallen in his net and its heavy weight had made it difficult to pull the net. The rejoiced fisherman removed his shirt, dived in the sea and pulled out the net with much effort. However, the fisherman was disappointed to find a dead jackass in his net, which had torn the net. The fisherman exclaimed in grief, “It seems, there is no justice left in the world of Allah!”
+
+After a while, he overcame his grief and disappointment and cast his net again in the name of Allah. When he tried to fetch it out, the net appeared heavier than before and it refused to move up. The fisherman was again hopeful of getting a big fish in his net., He at once dived down and fetched the net out. However, the fisherman was shattered to see the sight. There was a big mud filled pitcher trapped inside the net. The fisherman pleaded with the Almighty his bad luck and plight. He then prayed to the God and cast his net for the third time. This time, when the fisherman pulled out his net, he found it filled with potsherds and broken glass pieces.
+
+The fisherman’s sorrow knew no hound. With eyes fixed up towards the heaven, he complained, “O Almighty, you know that I cast my net only four times a day. I have already thrown it thrice and you did not give me anything. 0 Allah the great, kindly provide me at least something for my daily bread this time.”
+
+Saying like this, the morose fisherman cast his net for the fourth time. The net sank and the fisherman allowed it to settle. After waiting for sometime, the fisherman tried to pull it up. However, even after applying all his might, he could not bring it out. The annoyed fisherman cried aloud and said, “In the court of Allah there is no justice and power remained!”
+
+With all his disappointment and vexation, he dived to bring his net up. Finally, he brought it out and found a cucumber shaped jar of brass. The jar seemed filled with something. Its mouth was shut with a leaden cap, which was stamped with a seal ring of Lord Sulayman, the son of David.
+
+The fisherman rejoiced to find it. He thought, “If I sell this wonderful jar in the brass-market, it will certainly fetch me ten golden dinars.” He further thought, “Before selling the jar in the market, I must open it and see the content inside.”
+
+The fisherman then took a knife and put his effort to loose the lid from the jar. He worked on it till he could remove the lid from the jar. However, he was greatly surprised to find nothing inside. As he was wondering over the empty jar, a conspicuous sight jolted him. A thick vapour started coming out of the jar and it spiralled towards the high sky. The thick vapour condensed and took the shape of a huge ifrit. So huge was he that his head was touching the cloud while his feet were on the ground. He looked horrifying. His head was like a dome, hands looked similar to pitchforks, legs resembled very tall poles, mouth was enormous as a cave, teeth were like big boulders and two eyes appeared like burning lamps. The poor fisherman trembled with fear. For a few moments, he could not understand what to do and what to say. The ifrit looked at the fisherman and cried, “Sulayman, you arc not God but only the prophet of God. O prophet, please do not slay me. I will not say or do anything against you.”
+
+The fisherman said. ‘O Ifrit! Sulayman, the prophet of Allah whom you arc calling had died one thousand and eight hundred years ago. Our world is in its last stage. Tell me your story and apprise me of the reason for which von entered into this jar.”
+
+When the Satan heard the fisherman, he said, “There is only one God in this world. O Fisherman, cheer up for a while and be prepared to die.”
+
+The fisherman replied, “What harm have I done to you for which you want to kill me? I rescued you from the deep sea and freed you from this jar.”
+
+But the evil spirit said, “Tell me only how do you want to die?”
+
+The fisherman begged, “Please tell me what my fault is.”
+
+The ifrit said, “O Fisherman! I will tell you my story.” The jinni then began his tale – Once I spoke and worked against Sulayman, David’s son. The prophet sent his Wazir Asaf to arrest me. When he brought me before Sulayman, he asked me to abide by his order, which I refused. So he took a jar, closed me inside and sealed it with the mark of his name. He then ordered the jar to be thrown in the middle of the sea. In the deep ocean, I remained there for hundred years. During this period of time, I said to myself that whoever rescued me from the sea and freed me from the jar, I would make him rich forever. However, no one came to free me. When the next century, began, I vowed, “I shall present the whole world’s riches to the person who frees me.” Still no one rescued me and I remained lying in this deep ocean for the next four hundred years. After that, I decided, “I shall fulfil any one wish of the person, who comes to set me free.” Yet, no one came and I remained there in the deep ocean.
+
+My fury knew no bound when no one came to rescue me for such a long period of time. In the state of extreme rage, I said, “Now onwards whoever frees me from this jail, will be killed by me. But he will have the death of his choice.” Now you have freed me after one thousand eight years. So as per my vow, be prepared for getting slain by me. However, O Fisherman, you can avail of your choicest death.”
+
+Hearing the story of ifrit, the fisherman said, “O Jinni, I did not come to free you. Do not slay me. Please allow me to live my life. If you kill me, God will send some one else to slay you.”
+
+The ifrit replied, “There is no use talking on this issue. In any case, you have to die. Just tell me which way you would prefer to die.”
+
+The poor fisherman tried many ways to persuade the jinni and begged for his life several times but the ifrit was not ready to bestow any kindness. He was adamant on slaying the fisherman. Seeing it futile to expect any sympathy from the jinni, the fisherman thought, “He is a jinni, whereas I am a human being. Allah has bestowed us the power to outwit any other creature with our shrewdness. I must use my intelligence and get rid of him.”
+
+The fisherman said. “Are you all set to slay me?” The jinni answered in affirmation. The fisherman again asked, on the name of God. I ask you a question and expect an honest answer from you.” The ifrit asked the fisherman to be quick in his question. The fisherman said, “O Jinni, I’ve a serious doubt that you were ever inside this small jar, which cannot even accommodate your hand or foot or any part of your body. How could a huge jinni like you remained inside this small jar?” The ifrit replied, “Don’t you believe that I was inside this small jar?” The fisherman said, “No, I can’t! I can believe only when I see you inside with my own eyes.”
+
+When jinni heard this, he at once transformed himself into vapour and entered into the jar gradually. The moment, the fisherman saw the ifrit fully inside, he hastily picked up the leaden cap and closed the mouth of the jar, thus seizing the jinni inside. The trapped jinni tried to escape from there but Solomon’s ring did not allow him.
+
+After realising that he was outwitted by the fisherman, the ifrit begged, “0 fisherman allow me to go!” The fisherman replied, “No, Never! You are a devil. I’ll throw you back into the sea so that you can live there till the judgement day.”
+
+Ifrit said, “O fisherman, please open the jar. I’ll make you affluent.” The fisherman replied, “You liar, I can’t believe you. My position with you is similar to that of the Wazir of King Yuman, who was with the sage Duban.” The ifrit asked, “Who were they and what was their story?” The fisherman began his tale.
+.';
+	
+	
+?>
+<br > <br> </br>
+<a href = "logout.php">LOGOUT</a>
+<style>
+
+body{
+	background-image: url("violet.jpg");
+	font-size: 14px;
+	text-align: center;
+    text-color:white;
+  font-family:times new roman;
+	
+}
+h1{
+	
+	text-align: center;
+	font-family:arial;
+	color: black;
+	
+	}
+div{
+	<style>
+
+
+}
+</style>
+

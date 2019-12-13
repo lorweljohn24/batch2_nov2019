b5507bfa87ea46fc7a855f94d37443e6e43ff70f
MAYKILCIELO04 <cielomichael04@gmail.com>
2019-12-13 15:56:44 +0800

    CIELO
    FINAL UPDATE

---
+++ GROUP 2/CIELO/FINAL UPDATED/next1.php
@@ -0,0 +1,99 @@
+<html>
+<title>HTML Tutorial</title>
+<body>  
+
+
+
+<?php
+	session_start();
+	
+	echo '<h1 class = "text-center ">A Service of Love</h1>';
+	
+	$string = array(
+	
+  array(
+  "title" => "",
+  "story" => " 
+  simplest manners! Only eighteen years old. I'm to give three
+lessons a week; and, just think, Joe! $5 a lesson. I don't mind it a
+bit; for when I get two or three more pupils I can resume my
+lessons with Herr Rosenstock. Now, smooth out that wrinkle
+between your brows, dear, and let's have a nice supper.'
+'That's all right for you, Dele,' said Joe, attacking a can of peas
+with a carving knife and a hatchet, 'but how about me? Do you
+think I'm going to let you hustle for wages while I philander in the
+regions of high art? Not by the bones of Benvenuto Cellini! I
+guess I can sell papers or lay cobblestones, and bring in a dollar or
+two.'
+Delia came and hung about his neck.
+'Joe, dear, you are silly. You must keep on at your studies. It is
+not as if I had quit my music and gone to work at something else.
+While I teach I learn. I am always with my music. And we can live
+as happily as millionaires on $15 a week. You mustn't think of
+leaving Mr. Magister.'
+'All right,' said Joe, reaching for the blue scalloped vegetable
+dish. 'But I hate for you to be giving lessons. It isn't Art. But
+you're a trump and a dear to do it.'
+'When one loves one's Art no service seems too hard,' said
+Delia.
+'Magister praised the sky in that sketch I made in the park,' said
+Joe. 'And Tinkle gave me permission to hang two of them in his
+window. I may sell one if the right kind of a moneyed idiot sees
+them.'
+'I'm sure you will,' said Delia sweetly. 'And now let's be thankful
+for General Pinkney and this veal roast.'
+During all of the next week the Larrabees had an early breakfast. Joe was enthusiastic about some morning-effect sketches he
+was doing in Central Park, and Delia packed him off breakfasted,
+coddled, praised, and kissed at seven o'clock. Art is an engaging
+mistress. It was most times seven o'clock when he returned in the
+evening.
+At the end of the week Delia, sweetly proud but languid, triumphantly tossed three five-dollar bills on the 8 by 10 (inches)
+centre table of the 8 by 10 (feet) flat parlour.
+'Sometimes,' she said, a little wearily, 'Clementina tries me. I'm
+afraid she doesn't practise enough, and I have to tell her the same
+things so often. And then she always dresses entirely in white, and
+that does get monotonous. But General Pinkney is the dearest old
+man! I wish you could know him, Joe. He comes in sometimes 
+24 O HENRY - 100 SELECTED STORIES
+when I am with Clementina at the piano - he is a widower, you
+know - and stands there pulling his white goatee. And how are
+the semiquavers and the demi-semiquavers progressing? he
+always asks .",
+		"page" => 1
+		
+	),
+);
+
+echo "A Service of Love" . $string[0] ["story"]; 
+	
+?>
+<br></br>
+<div>
+<a href = "next2.php">Next...2</a>
+<a href = "next.php">previous</a>
+</div>
+
+</body>
+</html>
+<style>
+body{
+	background-image: url("22.jpg");
+	font-size:30px;
+	text-align:center;
+	color:;
+	padding: 20px;
+	margin: 10px;
+}
+h1{
+	text-align:center;
+	color:pink;
+}
+
+
+td { 
+    border:0px;
+	padding: 10px;
+}
+</style>
+
+
